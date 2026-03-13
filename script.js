599497047a32ec6c4cfc32380d58be193305e05d
henryvo2k4 <vodunghlhk@gmail.com>
2026-03-13 15:56:52 +0700

    update code roadmap v3

--- script.js
+++ script.js
@@ -692,22 +692,20 @@ geocoder.on("markgeocode", function (e) {
 
 });
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+const supabaseUrl = "https://sweqvobmlntyhyeuurfr.supabase.co";
+const supabaseKey = "sb_publishable_xsqRVFRoQSh0c9wzwc5vxA_Hw9aj9fF";
 
-const supabaseUrl = 'https://sweqvobmlntyhyeuurfr.supabase.co'
-const supabaseKey = 'sb_publishable_xsqRVFRoQSh0c9wzwc5vxA_Hw9aj9fF'
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-async function testDB(){
+async function testDB() {
 
 const { data, error } = await supabase
-.from('road_events')
-.select('*')
+.from("road_events")
+.select("*");
 
-console.log(data)
-console.log(error)
+console.log("DATA:", data);
+console.log("ERROR:", error);
 
 }
 
-testDB()
+testDB();
